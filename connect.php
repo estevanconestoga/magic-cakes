2bb27b2586d40b587bf3e5d219d3848f0f93929e
Fariba83 <84359750+Fariba83@users.noreply.github.com>
2021-12-04 20:31:00 -0500

    database connect is put in one folder called "connect.php"

--- connect.php
+++ connect.php
@@ -1,23 +1,18 @@
-<?php
+ <?php
+
+/	function prepare_string($sql_connection, $string) {
+		$string = mysqli_real_escape_string($sql_connection, trim($string));
+		return $string;
+	}
+
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'magiccake');
+
+	$sql_connection = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+		OR die('Could not connect to MySQL: ' . mysqli_connect_error());
+	mysqli_set_charset($sql_connection, 'utf8');
 
 
-
-
- $sql_connection = mysqli_connect('localhost', 'root', '', 'magiccake');
-
-
-
-    if($sql_connection->connect_error){
-        die('connection failed : '.$sql_connection->connect_error);
-    }
-
-  
-
-
-
-
-
-
-
-
-?> 
+  ?>  
