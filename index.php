6fbc01316a27f620b425bcdd544b3e169ed52bce
OlyaLtrend <84355321+OlyaLtrend@users.noreply.github.com>
2021-12-07 22:24:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -111,49 +111,11 @@
       </button>
     </div>
     <script src="menu.js"></script>
-
-    <footer class="container">
-      <div class="container-fluid text-center text-md-left">
-        <div class="row">
-          <div class="col-md-6 mt-md-0 mt-3">
-            <h5 class="text-uppercase footer-content-h ft-nav-title">Subscribe and Save</h5>
-            <p class="footer-content-b">Subscribe to our Newsletter and receive <br> offers difrectly to you inbox.</p>
-            <form class="form-inline">
-              <div class="form-group form-footer mx-sm-3 mb-2">
-                <label for="inputPassword2" class="sr-only">Your Email</label>
-                <?php
-      
-
-                ?>
-                <input type="text" name="email"class="form-control form-footer" id="subscribe_email" placeholder="Your Email">
-              </div>
-              <button type="submit" name="subscribe" class="btn btn-primary mb-2">Subscribe</button>
-            </form>
-          </div>
-          <hr class="clearfix w-100 d-md-none pb-3">
-
-          <div class="col-md-6 mb-md-0 mb-3">
-
-            <h5 class="text-uppercase ft-nav-title">Navigation</h5>
-
-            <ul class="list-unstyled">
-              <li>
-                <a href="index.php" class="ft-nav-item">Home</a>
-              </li>
-              <li>
-              <a class="ft-nav-item" href="contact.php">Menu</a>
-            </li>
-                <a href="about.php" class="ft-nav-item">About</a>
-              </li>
-              <li>
-                <a href="contact.php" class="ft-nav-item">Contact Us</a>
-              </li>
-            </ul>
-
-          </div>
-        </div>
-      </div>
-    </footer>
+    
+      <?php
+        require "footer.php";
+      ?>
+    
   </body>
 
 </html>
